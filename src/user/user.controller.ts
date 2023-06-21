566fe2aab719bb89ef4a374c5789eed1e386eebd
Mohamed Yasser <51481220+shnki@users.noreply.github.com>
2023-06-22 01:06:10 +0300

    adding more documentation via swagger

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -15,6 +15,8 @@ import {
   ApiBadRequestResponse,
   ApiCreatedResponse,
   ApiTags,
+  ApiOperation,
+  ApiResponse,
 } from '@nestjs/swagger';
 
 @ApiTags('User')
@@ -24,16 +26,71 @@ export class UserController {
 
   @Post('signup')
   @UsePipes(new ValidationPipe())
-  @ApiCreatedResponse({ description: 'User created' })
-  @ApiBadRequestResponse({
-    description: 'Error , Maybe user already signed up',
+  @ApiCreatedResponse({
+    description: 'User created',
+    schema: {
+      example: {
+        name: 'matt murdock',
+        email: 'username@example.com',
+        state: 'Nevada',
+        city: 'Eureka',
+        id: '31',
+      },
+    },
   })
+  @ApiBadRequestResponse({
+    description:
+      'Error , Maybe user already signed up or body is missing a field',
+    schema: {
+      example: {
+        message: 'invalid data',
+        error: 'Bad Request',
+        statusCode: 400,
+      },
+    },
+  })
+  @ApiOperation({ summary: 'signs up a user ' })
   async signup(@Body() userData: SignupDto) {
     const response = await this.userservice.signUp(userData);
     return response;
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'get user by id' })
+  @ApiResponse({
+    status: 200,
+    description: 'User returned',
+    schema: {
+      example: {
+        name: 'matt murdock',
+        email: 'daredevil@hotmail.com',
+        state: 'Nevada',
+        city: 'Eureka',
+      },
+    },
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'User not found',
+    schema: {
+      example: {
+        message: 'Cannot GET /user/',
+        error: 'Not Found',
+        statusCode: 404,
+      },
+    },
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'invalid id',
+    schema: {
+      example: {
+        message: 'Validation failed (numeric string is expected)',
+        error: 'Bad Request',
+        statusCode: 400,
+      },
+    },
+  })
   async getUser(@Param('id', ParseIntPipe) userId: number) {
     const user = await this.userservice.getUser(userId);
     if (!user) {
